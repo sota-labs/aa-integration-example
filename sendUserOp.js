9d05be690bde205a99ccf04fb8f26f07107ba8b0
sotatek-tinha <tin.ha@sotatek.com>
2024-03-26 14:14:06 +0700

    chore: update example data

--- sendUserOp.js
+++ sendUserOp.js
@@ -7,8 +7,8 @@ async function main() {
 
   const method = "POST";
   const apiPath = "/api/backend/user-operations/send";
-  const apiKey = ""; // TODO: add apiKey, e.g. api-key
-  const secretKey = ""; // TODO: add secretKey, e.g. secret-key
+  const apiKey = "tinbot-test-api"; // TODO: add apiKey, e.g. api-key
+  const secretKey = "tinbot-secret-key"; // TODO: add secretKey, e.g. secret-key
 
   const abi = ["function transfer(address to, uint amount)"];
   const iface = new Interface(abi);
@@ -19,12 +19,12 @@ async function main() {
 
   try {
     const bodyData = {
-      network: "network", // TODO: add network, e.g. "ETH_SEPOLIA", "BSC_TESTNET", "AVALANCHE_TESTNET", "BLAST_SEPOLIA", "SOLANA_DEVNET"
-      userId: "userId", // TODO: add your userId, e.g. "123456"
-      walletAddress: "walletAddress", // TODO: add wallet address created by your bot
-      to: "to", // TODO: add to address, here is avabot router address
+      network: "BSC_TESTNET", // TODO: add network, e.g. "ETH_SEPOLIA", "BSC_TESTNET", "AVALANCHE_TESTNET", "BLAST_SEPOLIA", "SOLANA_DEVNET"
+      userId: "6767187788", // TODO: add your userId, e.g. "123456"
+      walletAddress: "0x8224DEE628feEb705Aa79880d8A7E44838F427c4", // TODO: add wallet address created by your bot
+      to: "0x3438b917f12d674F689DB30D8E3DF209A240288a", // TODO: add to address, here is avabot router address
       data: encodedData,
-      value: "value", // TODO: add value you want to send in wei, e.g. "1000000000000000"
+      value: "1000000000000000", // TODO: add value you want to send in wei, e.g. "1000000000000000"
     };
     const signature = generateSignature(method, apiPath, bodyData, secretKey);
 
@@ -37,6 +37,8 @@ async function main() {
     );
 
     console.log("Response:", response.data);
+
+    // successful tx: https://testnet.bscscan.com/tx/0x309de4712308a3e6c89fff08662e22c4c524c9a5c508ebd548b5d21c00830d3a
   } catch (error) {
     console.error("Error:", error.response.data);
   }
